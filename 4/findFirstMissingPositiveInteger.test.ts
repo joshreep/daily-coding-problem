7526e20753c0a552ce537e9c7150ad1e96fb6905
Josh Reep <joshreep@gmail.com>
2019-12-03 08:57:11 -0500

    Adds additional tests for problem 4

--- 4/findFirstMissingPositiveInteger.test.ts
+++ 4/findFirstMissingPositiveInteger.test.ts
@@ -4,5 +4,8 @@ describe('findFirstMissingPositiveInteger', () => {
     it('should return the first missing positive integer in linear time and constant space', () => {
         expect(findFirstMissingPositiveInteger([3, 4, -1, 1])).toBe(2)
         expect(findFirstMissingPositiveInteger([1, 2, 0])).toBe(3)
+        
+        expect(findFirstMissingPositiveInteger([1, 2, 0, 1])).toBe(3)
+        expect(findFirstMissingPositiveInteger([1, 2, 0, 1, 2, -1, -100])).toBe(3)
     })
 })
