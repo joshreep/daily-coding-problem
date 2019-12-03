9e571aa844e5fc03c685dc8653a97566f8db87ee
Josh Reep <joshreep@gmail.com>
2019-12-03 08:56:58 -0500

    Adds logic for problem 4

--- 4/findFirstMissingPositiveInteger.ts
+++ 4/findFirstMissingPositiveInteger.ts
@@ -1,3 +1,22 @@
-const findFirstMissingPositiveInteger = (array: number[]) => {}
+const findFirstMissingPositiveInteger = (array: number[]) => {
+    const positiveIntegersArray = array.filter(value => value >= 1)
+    const uniqueArray = positiveIntegersArray.filter(
+        (value, index) => positiveIntegersArray.indexOf(value) === index
+    )
+    const sortedArray = uniqueArray.sort()
+    let result = 0
+
+    sortedArray.forEach((value, index) => {
+        if (result === 0) {
+            if (value !== index + 1) result = index + 1
+        }
+    })
+
+    if (result === 0) {
+        result = sortedArray[sortedArray.length - 1] + 1
+    }
+
+    return result
+}
 
 export default findFirstMissingPositiveInteger
