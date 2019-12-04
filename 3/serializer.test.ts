5fa70f933b85c30648ef39aa75d9572f4e191363
Josh Reep <jreep@startchurch.com>
2019-12-04 09:18:13 -0500

    Adds breaking tests

---
+++ 3/serializer.test.ts
@@ -0,0 +1,19 @@
+import Node from './Node'
+import { serialize, deSerialize } from './serializer'
+
+const node = new Node('root', new Node('left', new Node('left.left')), new Node('right'))
+const string = '{"val": "root", "left": {"val": "left", "left": {"val": "left.left"}}, "right": {"val": "right"}}'
+
+describe('serializer', () => {
+    it('should serialize a Node to a string', () => {
+        expect(serialize(node)).toBe(string)
+    })
+
+    it('should de-serialize a string back to a Node', () => {
+        expect(deSerialize(string)).toStrictEqual(node)
+    })
+
+    it('should serialize and de-serialize a Node there and back again', () => {
+        expect(deSerialize(serialize(node)).left.left.val).toBe('left.left')
+    })
+})
---
+++ 3/serializer.ts
@@ -0,0 +1,9 @@
+import Node from './Node'
+
+export const serialize = (root: Node): string => {
+    return ''
+}
+
+export const deSerialize = (string: string): Node => {
+    return new Node('')
+}
