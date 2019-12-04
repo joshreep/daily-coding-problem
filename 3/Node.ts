ac698e80d72a91bd6c08f9d28d97f6f4a8099b95
Josh Reep <jreep@startchurch.com>
2019-12-04 08:40:09 -0500

    Adds Node class to problem 3

---
+++ 3/Node.ts
@@ -0,0 +1,11 @@
+class Node {
+    constructor(val: string, left?: Node, right?: Node) {
+        this.val = val
+        this.left = left
+        this.right = right
+    }
+
+    val: string
+    left?: Node
+    right?: Node
+}
