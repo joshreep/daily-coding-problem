5eb2310af8ffbfedf8a8a3ac0240c9bc0f762118
Josh Reep <josh.reep@sstid.com>
2020-04-06 07:54:58 -0400

    all tests passing

--- 132/HitCounter.ts
+++ 132/HitCounter.ts
@@ -9,13 +9,27 @@ export default class HitCounter {
         return this._hits
     }
 
-    set hits(hits) {
-        this._hits = hits
+    set hits(_hits) {
+        throw new Error(
+            'hits cannot be explicitly set. Call the record method to set hits or pass in array to the constructor',
+        )
     }
 
-    record(timestamp: number) {}
+    record(timestamp: number) {
+        this._hits.push(timestamp)
+    }
 
-    total() {}
+    total() {
+        return this._hits.length
+    }
 
-    range(lower: number, upper: number) {}
+    range(lower: number, upper: number) {
+        if (lower > upper) {
+            throw new Error(
+                `lower boundary must be less than upper boundary. ${lower} is greater than ${upper}`,
+            )
+        }
+
+        return this._hits.filter((hit) => hit >= lower && hit <= upper).length
+    }
 }
