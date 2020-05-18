f926613f249d9c502e9c5bcf8c51ccf549b7419e
Josh Reep <josh.reep@sstid.com>
2020-05-18 10:28:08 -0400

    makes code pass for problem 173

--- 173/flatten.ts
+++ 173/flatten.ts
@@ -1,5 +1,23 @@
 export type StringKeyObject<T = any> = { [key: string]: T }
 
 export default function flatten<T>(input: StringKeyObject<T>): StringKeyObject<T> {
-    return {}
+    const inputEntries = Object.entries(input)
+
+    if (inputEntries.every(([, value]) => value === null || typeof value !== 'object')) {
+        return input
+    }
+
+    const result: StringKeyObject<T> = {}
+
+    inputEntries.forEach(([key, value]) => {
+        if (value === null || typeof value !== 'object') {
+            result[key] = value
+        } else {
+            Object.entries(value).forEach(([subKey, subValue]) => {
+                result[`${key}.${subKey}`] = subValue
+            })
+        }
+    })
+
+    return flatten(result)
 }
