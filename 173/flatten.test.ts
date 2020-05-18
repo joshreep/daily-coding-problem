6c696e2a5dd055d2450d299f89eb35c353de0b28
Josh Reep <josh.reep@sstid.com>
2020-05-18 10:27:54 -0400

    updates tests to include multiple scenarios

--- 173/flatten.test.ts
+++ 173/flatten.test.ts
@@ -1,12 +1,30 @@
 import flatten, { StringKeyObject } from './flatten'
 
 describe('flatten', () => {
-    test('should flatten a nested dictionary', () => {
-        const input: StringKeyObject<any> = { key: 3, foo: { a: 5, bar: { baz: 8 } } }
-        const expected: StringKeyObject<any> = { key: 3, 'foo.a': 5, 'foo.bar.baz': 8 }
-
-        Object.keys(expected).forEach((key) => {
-            expect(flatten(input)[key]).toEqual(expected[key])
+    const scenarios: { input: StringKeyObject; expected: StringKeyObject }[] = [
+        {
+            input: { key: 3, foo: { a: 5, bar: { baz: 8 } } },
+            expected: { key: 3, 'foo.a': 5, 'foo.bar.baz': 8 },
+        },
+        {
+            input: {
+                key: 3,
+                foo: { a: 5, bar: { baz: 8, thing: { thang: 49, that: { this: 2 } } } },
+            },
+            expected: {
+                key: 3,
+                'foo.a': 5,
+                'foo.bar.baz': 8,
+                'foo.bar.thing.thang': 49,
+                'foo.bar.thing.that.this': 2,
+            },
+        },
+    ]
+    scenarios.forEach((scenario) => {
+        test('should flatten a nested dictionary', () => {
+            Object.keys(scenario.expected).forEach((key) => {
+                expect(flatten(scenario.input)[key]).toEqual(scenario.expected[key])
+            })
         })
     })
 })
